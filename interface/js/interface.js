62fff3b73a494bbee1aaac6f4b45f104bb968789
minhquan <dinhbaminhquan69@gmail.com>
2023-08-03 12:47:21 +0700

    push IF

---
+++ interface/js/interface.js
@@ -0,0 +1,50 @@
+//======================= Import ======================================
+// import APIs
+import * as apiMethod from "./../src/services/productsAPI";
+
+// default export
+import consObject from "../src/models/products";
+
+// ======utilities======
+function getElement(selector) {
+  return document.querySelector(selector);
+}
+// ====================== Global function================================
+
+// Get information
+function getInfoProducts() {
+  apiMethod
+    .apiGetProducts()
+    .then((response) => {
+      console.log(response.data);
+      displayProducts(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// display
+function displayProducts(products) {
+  let contentHTML = products.reduce((result, value) => {
+    let itemProduct = new consObject(
+      value.id,
+      value.name,
+      value.price,
+      value.image,
+      value.type
+    );
+    return (
+      result +
+      `
+        <div class="col-4 product-item">
+            <div product-img>
+                <img src"${products.image}" alt="phone"/>
+            </div>
+            <div class="product-name">${products.name}</div>
+        </div>
+      `
+    );
+  }, " ");
+  document.getElementById("list-product").innerHTML = contentHTML;
+}
