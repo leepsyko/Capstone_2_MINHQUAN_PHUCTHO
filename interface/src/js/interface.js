01352eaab19f52fd95b451f607df0758fc9b2bf3
minhquan <dinhbaminhquan69@gmail.com>
2023-08-10 18:45:16 +0700

    len 10/8

--- interface/src/js/interface.js
+++ interface/src/js/interface.js
@@ -10,25 +10,30 @@ function getElement(selector) {
   return document.querySelector(selector);
 }
 
+function getElements(selector) {
+  return document.querySelectorAll(selector);
+}
 // ====================== Global function================================
 
+// DOM
+let quantity = getElement(".quantity-product");
+let bodyCartProduct = getElement("#offcanvas-body-product");
 getInfoProducts();
 
 // Get information
-function getInfoProducts() {
+export function getInfoProducts() {
   apiMethod
     .apiGetProducts()
     .then((response) => {
-      console.log(response.data);
       displayProducts(response.data);
     })
     .catch((error) => {
       console.log(error);
     });
 }
-
+const carts = [];
 // display
-function displayProducts(products) {
+export function displayProducts(products) {
   let contentHTML = products.reduce((result, value, index) => {
     let itemProduct = new consObject(
       value.id,
@@ -41,6 +46,9 @@ function displayProducts(products) {
       value.desc,
       value.type
     );
+
+    let productsJson = JSON.stringify(products);
+    localStorage.setItem("productsJson", productsJson);
     return (
       result +
       `
@@ -60,7 +68,7 @@ function displayProducts(products) {
           </div>
           <div>
             <button class="btn btn-secondary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Info</button>
-            <button class="btn btn-light">Add</button>
+            <button class="btn btn-light addCartProduct" data-id="${itemProduct.id}" data-img="${itemProduct.image}" data-name="${itemProduct.name}" data-price="${itemProduct.price}" data-index=${index}>Add</button>
           </div>
         </div>
       </div>
@@ -107,8 +115,99 @@ function displayProducts(products) {
     );
   }, " ");
 
-  // getElement(".deletePro").onclick = (event) =>{
-  //   console.log(event.target)
-  // }
   document.getElementById("list-product").innerHTML = contentHTML;
+  let productStorangeJson = localStorage.getItem("productsJson");
+  let productsOb = JSON.parse(productStorangeJson);
+  getElements(".addCartProduct").forEach((element) => {
+    element.onclick = (event) => {
+      const target = event.target;
+      const id = target.getAttribute("data-id");
+      const name = target.getAttribute("data-name");
+      const image = target.getAttribute("data-img");
+      const price = target.getAttribute("data-price");
+      const index = target.getAttribute("data-index");
+      if (carts[index] == null) {
+        // copy product form list to list card
+        carts[index] = JSON.parse(JSON.stringify(productsOb[index]));
+        carts[index].quantity = 1;
+        // getElement(".offcanvas-body-notice").style.display = "none";
+        // getElement("#offcanvas-body-product").style.display = "block";
+      } else {
+        if (carts[index]) {
+          carts[index].quantity++;
+          carts[index].price = carts[index].quantity * productsOb[index].price;
+          // getElement(".offcanvas-body-notice").style.display = "none";
+          // getElement("#offcanvas-body-product").style.display = "block";
+        }
+      }
+      reLoadCart();
+
+      // document.querySelectorAll(".plusButton").forEach((button) => {
+      //   button.addEventListener("click", (event) => {
+      //     let b = event.target.getAttribute("data-index2");
+      //     console.log(carts[b]);
+      //     carts[b].quantity++;
+      //     carts[b].price = carts[b].quantity * productsOb[b].price;
+      //     reLoadCart();
+      //   });
+      // });
+      // document.querySelectorAll(".minusButton").forEach((button) => {
+      //   button.addEventListener("click", (event) => {
+      //     let b = event.target.getAttribute("data-index2");
+      //     carts[b].quantity--;
+      //     carts[b].price = carts[b].quantity * productsOb[b].price;
+      //     console.log(carts[b].quantity);
+      //     if (carts[b].quantity == 0) {
+      //       delete carts[b];
+      //     }
+
+      //     reLoadCart();
+      //   });
+      // });
+      getElement(".quantity-product").style.display = "block";
+    };
+  });
+}
+
+getElement(".offcanvas-body").onclick = (event) => {
+  let productStorangeJson = localStorage.getItem("productsJson");
+  let productsOb = JSON.parse(productStorangeJson);
+  const target = event.target;
+  let id = target.getAttribute("id");
+  let idData = target.getAttribute("data-id");
+  getElement(`#plusButton-${idData}`).addEventListener("click", (event) => {
+    let b = event.target.getAttribute("data-index2");
+    carts[b].quantity++;
+    carts[b].price = carts[b].quantity * productsOb[b].price;
+    reLoadCart();
+  });
+};
+
+function reLoadCart() {
+  let bodyProduct = getElement("#offcanvas-body-product");
+  bodyCartProduct.innerHTML = "";
+  let count = 0;
+  let totalPrice = 0;
+  carts.forEach((value, index) => {
+    count = count + value.quantity;
+    totalPrice = totalPrice + value.price * 1;
+    if (value != null) {
+      getElement(".offcanvas-body-notice").style.display = "none";
+      let newDiv = document.createElement("div");
+      newDiv.innerHTML = `
+          <div class="d-flex align-items-center my-2">
+              <img src=${value.image} width="50px" height="50px" />
+              <h5 class="mx-3 m-0">${value.name}</h5>
+              <div class="d-flex">
+                <button class="btn btn-light minusButton" id="minusButton-${value.id}" data-id="${value.id}" data-index2="${index}">-</button>
+                <input id="quantity-${value.id}" value="${value.quantity}" class="cart-quantity"/>
+                <button class="btn btn-light plusButton" id="plusButton-${value.id}" data-id="${value.id}" data-value="${value.quantity}" data-index2="${index}")">+</button>
+              </div>
+              <h6 class="mx-2 m-0">${value.price} Đ</h6>
+              <button class="btn btn-danger text-center">X</button>
+          </div>
+      `;
+      bodyProduct.appendChild(newDiv);
+    }
+  });
 }
